48df06b7cc2d501ed5c8978ae2cbe9f174eb7294
xkocma09 <xkocma09@stud.fit.vutbr.cz>
2023-03-16 16:19:07 +0100

    feat: argument validation and server setup

--- ipkcpc.c
+++ ipkcpc.c
@@ -8,7 +8,8 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include<string.h>
+#include <strings.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <getopt.h>
@@ -18,34 +19,62 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define COMP_ARG_NUM 6
+#define ARG_NUM 6
+#define WELL_KNOWN_PORTS 1023 //number of well known ports
+#define MAX_PORT_NUM 65353 //max port number for tcp/udp protocols
 
 extern int optopt, opterr, optind;
+extern char *optarg;
 
 void print_help(){
   printf("help is being printed\n");
 }
 
 int main(int argc, char** argv){
-    int c;
-    opterr = 0;
-
-    if(argc != (COMP_ARG_NUM + 1)){
+    int c, port_num, mode;
+    char *host_name = NULL;
+    struct hostent *ipk_server;
+    struct sockaddr_in server_addr;
+//////////////////////////////////////////
+    if(argc != (ARG_NUM + 1)){
       print_help();
       return 1;
     }
+//////^^kontrola poctu argumentu^^////////
 
+    opterr = 0;
+/////////////////////////////////////////////////////////
     while ((c = getopt(argc, argv, "h:p:m:")) != -1){
     switch (c)
       {
       case 'h':
-        printf("host:\n");
+        host_name = malloc(sizeof(optarg));
+        strcpy(host_name, optarg);
         break;
       case 'p':
-        printf("port:\n");
+        port_num = atoi(optarg);
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        if(port_num <= WELL_KNOWN_PORTS || port_num >= MAX_PORT_NUM){
+          fprintf(stderr, "ERROR: invalid port number '%d' must be within <1024, 65_353>\n", port_num);
+          if(host_name != NULL)
+            free(host_name);
+          exit(1);
+        }
+        ////////////////////////////////^^kontrola cisla portu^^///////////////////////////////////////
         break;
       case 'm':
-        printf("mode:\n");
+        if(!strcmp(optarg, "udp"))
+          mode = 0;
+        else if(!strcmp(optarg, "tcp"))
+          mode = 1;
+        else{
+        /////////////////////////////////////////////////////////////////////////////////
+          fprintf(stderr, "ERROR: unknown mode expected [udp tcp] given %s\n", optarg);
+          if(host_name != NULL)
+            free(host_name);
+          exit(1);
+        ////////////////////////////^^neznamy protokol^^//////////////////////////////////
+        }
         break;
       case '?':
         if (optopt == 'h')
@@ -58,15 +87,47 @@ int main(int argc, char** argv){
           fprintf (stderr, "Unknown option `-%c'.\n", optopt);
         else
           fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-        return 1;
+
+        free(host_name);
+        exit(1);
       default:
-        return 1;
+
+        free(host_name);
+        exit(1);
       }
     }
-    
+////////////////^^zpracovani argumentu^^/////////////////
+
+///////////////////////////////////////
     if(optind < argc){
         fprintf(stderr, "Unexpected non-option arguments\n");
-        return 1;
+        if(host_name != NULL)
+          free(host_name);
+        exit(1);
     }
+///^^kontrola necekanych argumentu^^///
+
+////////////////////////////////////////////////////////////////////
+    if ((ipk_server = gethostbyname(host_name)) == NULL) {
+        fprintf(stderr,"ERROR: unknown hostname '%s'\n", host_name);
+        exit(1);
+    }
+//////////^^ziskani adresy sereru pomoci DNS sluzby^^///////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    bzero((char *)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    bcopy((char *)ipk_server->h_addr_list[0], (char *)&server_addr.sin_addr.s_addr, ipk_server->h_length);
+    server_addr.sin_port = htons(port_num);
+/////////////////////////////^^nastaveni cisla potru a IP adresy hosta^^////////////////////////////////////    
+    
+    /* tiskne informace o vzdalenem soketu */ 
+    printf("INFO: Server socket: %s : %d \n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+    if(mode)
+      printf("tcp\n");
+    else
+      printf("udp\n");
+    if(host_name != NULL)
+      free(host_name);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,8 +1,15 @@
 CC=gcc
 CFLAGS=-std=c99 -Wall -Wextra -pedantic -lm -fcommon
-.PHONY=run
+.PHONY=run clean valgrind
 
-run: ipkcpc
-	./ipkcpc -p port -m mode -h host
-ipkcpc: ipkcpc.c
-	$(CC) $(CFLAGS) -o ipkcpc ipkcpc.c 
+run: ipkcpc.out
+	./ipkcpc.out -p 2023 -m tcp -h localhost
+
+valgrind: ipkcpc.out
+	valgrind ./ipkcpc.out -p 2023 -m udp -h localhost
+
+ipkcpc.out: ipkcpc.c
+	$(CC) $(CFLAGS) -o ipkcpc.out ipkcpc.c 
+
+clean:
+	rm *.out *.o
