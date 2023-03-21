d5d249a9708e1908910c73b8fc555048edeb5723
xkocma09@stud.fit.vutbr.cz <xkocma09@stud.fit.vutbr.cz>
2023-03-21 07:37:16 +0000

    feat: tests tcp

--- test.sh
+++ test.sh
@@ -2,20 +2,20 @@
 #testy pro IPk projekt 1
 
 function echo_fail(){
-	echo -e "[\033[1;31mFail\033[0m]"
+	echo -e "          [\033[1;31mFail\033[0m]"
 }
 function echo_pass(){
-	echo -e "[\033[1;32mPass\033[1;0m]"
+	echo -e "          [\033[1;32mPass\033[1;0m]"
 }
 binfile='./ipkcpc.out';
 host='localhost';
 portudp='2023';
 porttcp='2024';
 
-touch result;
 
-function test(){ # $1 - name
-	echo "test $1";
+function testudp(){ # $1 - name
+	echo "test $1:";
+	echo "---------------";
 	wcount=$(cat `echo "testfiles/$1.in"` | `echo "$binfile -h $host -p $portudp -m udp"`| diff testfiles/`echo $1`.out - );
 	if [[ -z "$wcount" ]]
 	then
@@ -26,6 +26,23 @@ function test(){ # $1 - name
 	fi
 }
 
-test udp;
-test udpdivZ;
-test udpInvReq;
+function testtcp(){ # $1 - name
+	echo "test $1:";
+	echo "---------------";
+	wcount=$(cat `echo "testfiles/$1.in"` | `echo "$binfile -h $host -p $porttcp -m tcp"`| diff testfiles/`echo $1`.out - );
+	if [[ -z "$wcount" ]]
+	then
+		echo_pass;
+	else
+		cat `echo "testfiles/$1.in"` | `echo "$binfile -h $host -p $porttcp -m tcp"`;
+		echo_fail;
+	fi
+}
+
+testudp udp;
+testudp udpdivZ;
+testudp udpInvReq;
+
+testtcp tcp;
+testtcp tcpdivZ;
+testtcp tcpInvReq;
